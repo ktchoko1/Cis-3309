4a9a03c974bad2a31fbb4c97ab9569040930bc51
Kouasssi Tchokoto <tug78561@temple.edu>
2019-01-30 23:41:15 -0500

    Add code for calculating area and perimeter knowing the length and the width.

--- CalculateArea/CalculateArea/frmMain.cs
+++ CalculateArea/CalculateArea/frmMain.cs
@@ -19,6 +19,26 @@ namespace CalculateArea
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            // Get the length value from the user
+            double numLength = Convert.ToDouble(txtLength.Text);
+
+            // get the width value from the user
+            double numWidth = Convert.ToDouble(txtWidth.Text);
+
+            // Declair and initilize the area and the length
+            double numArea = 0;
+            double numPerimeter = 0;
+
+            // Calculate the area
+            numArea = numLength * numWidth;
+            txtArea.Text = Convert.ToString(numArea);
+
+            // Calculate the Perimeter
+
+            numPerimeter = 2 * (numLength + numWidth);
+            txtPerimeter.Text = Convert.ToString(numPerimeter);
+
+            txtLength.Focus();
 
         }
 
